dcb504f101c94a5803b62bcf7abdb5cb39520c2e
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-02-21 21:31:24 +0000

    L'ajout de annonce controleur.

--- lost-found-laravel/app/Models/annonce.php
+++ lost-found-laravel/app/Models/annonce.php
@@ -23,6 +23,8 @@ class annonce extends Model
     public function user(){
         return $this->belongsTo(User::class);
      }
+
+     
 public function comments()
 {
     return $this->hasMany(comment::class, 'id_annonce');

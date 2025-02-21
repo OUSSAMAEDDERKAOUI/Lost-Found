6fd9e57c06872278db49fade9b3c0eef478959e6
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-02-21 21:25:41 +0000

    L'ajout des méthodes annonce et user dans le modèle. Comment

--- lost-found-laravel/app/Models/comment.php
+++ lost-found-laravel/app/Models/comment.php
@@ -10,18 +10,14 @@ class comment extends Model
     use HasFactory;
     protected $fillable=['contenu','id_user','id_annonce'];
 
-  // Dans le modèle Comment
 public function annonce()
 {
     return $this->belongsTo(annonce::class, 'id_annonce');
 }
 
-// Comment.php
 public function user() {
-    return $this->belongsTo(User::class, 'id_user'); // Spécifie 'id_user' comme clé étrangère
+    return $this->belongsTo(User::class, 'id_user'); 
 }
-
     
 }
 
-  
